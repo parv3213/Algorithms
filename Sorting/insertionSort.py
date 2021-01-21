b7e20a5be8e24017f12d715fd1a50de044bcc206
Parv Garg <parv3213@gmail.com>
2021-01-21 21:18:58 +0530

    insertion sort

---
+++ Sorting/insertionSort.py
@@ -0,0 +1,18 @@
+# Insertion Sort
+
+def incertionSort (arr):
+    for i in range(1, len(arr)):
+        if arr[i-1] > arr[i]:
+            key = arr[i]
+            j = i-1
+            while j>=0 and key < arr[j]:
+                arr[j+1] = arr[j]
+                j -= 1
+            arr[j+1] = key
+    return arr
+
+# Driver code
+arr = [64, 34, 25, 12, 22, 11, 90] 
+print ("Sorted Array:")
+arr = incertionSort(arr)
+print (arr)
